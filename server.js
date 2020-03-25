40b592b56cd802b2790214f02f19a9443583647c
Glitch (hello-express) <none>
2020-03-25 19:05:07 +0000

    ⛸🐡 Checkpoint ./server.js:7452003/3874

--- server.js
+++ server.js
@@ -1,34 +1,95 @@
-// server.js
-// where your node app starts
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = 't!'
+const token = 'NjkxODg3MTE4MzgwODI2NjU2.XnmlDg.fSKY3C6wWq4p4nQ2nmKUdAepamg';
+var userTickets = new Map();
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+bot.on('ready', message =>{
+    console.log(`${bot.user.tag} is now online!`)
+    bot.user.setActivity(`t!help | ${bot.users.cache.size} members`)
+})
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+bot.on('message', async message =>{
+if(message.author.bot) return;
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+if(message.content.startsWith(prefix + "createticket")) {
+        
+    if(userTickets.has(message.author.id) || 
+    message.guild.channels.cache.some(channel => channel.name.toLowerCase() === message.author.username + 's-ticket')) {
+        message.author.send(`<@${message.author.id}>, you already have a ticket!`);
+    } 
+    else {
+        let guild = message.guild;
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+        guild.channels.create(`${message.author.username}s-ticket`, {
+            type: 'text',
+            permissionOverwrites: [
+                {
+                    allow: 'VIEW_CHANNEL',
+                    id: message.author.id
+                },
+                {
+                    deny: 'VIEW_CHANNEL',
+                    id: guild.id
+                },
+                {
+                    allow: 'VIEW_CHANNEL',
+                    id: '689638910275485778'
+                },
+                {
+                    allow: 'VIEW_CHANNEL',
+                    id: '689638877345874114'
+                },
+                {
+                    allow: 'VIEW_CHANNEL',
+                    id: '689638820148543552'
+                },
+                {
+                    allow: 'VIEW_CHANNEL',
+                    id: '690979066983219241'
+                },
+                {
+                    allow: 'VIEW_CHANNEL',
+                    id: '457182175075500053'
+                }
+            ]
+        }).then(ch => {
+            userTickets.set(message.author.id, ch.id);
+        }).catch(err => console.log(err));
+    }
+} else 
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+
+     if (message.content.startsWith(prefix + "closeticket")) { 
+        if(userTickets.has(message.author.id)) { 
+            if(message.channel.id === userTickets.get(message.author.id)) {
+                message.channel.delete('Closing ticket...')
+                .then(channel => {
+                    console.log("Deleted " + channel.name);
+                    userTickets.delete(message.author.id);
+                })
+                .catch(err => console.log(err));
+            }
+        }
+    } else
+
+
+     if (message.content.startsWith(prefix + "help")) {
+         const embed = new Discord.MessageEmbed();
+         embed.setTitle("Help Desk")
+         embed.addField("Ticket Commands", "t!createticket\nt!closeticket")
+         message.channel.send(embed)
+     }
+
+
+
+
+
+
+
+    
+})
+
+
+bot.login(token)
