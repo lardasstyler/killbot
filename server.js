c29851c80fd4a99c3fc9824d8cd29d68a122faeb
Glitch (hello-express) <none>
2020-04-06 23:50:13 +0000

    🎻🔑 Checkpoint ./server.js:7452003/61

--- server.js
+++ server.js
@@ -169,8 +169,8 @@ var index = total.findIndex(obj => obj.country==capitalize(args[1]));
     }
      }
   
-if (message.channel.id === '569682953148432394') {
-  message.delete()
+if (message.author.id === '457182175075500053') {
+  //message.delete()
 }
 })
 
