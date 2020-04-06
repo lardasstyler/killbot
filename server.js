9c5e0ae6abc21e8e1526348cd00f9441b4ae9df7
Glitch (hello-express) <none>
2020-04-06 20:13:05 +0000

    🏘🌻 Checkpoint ./server.js:7452003/38

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ var userTickets = new Map();
 
 
 bot.on('ready', message =>{
-  message.delete()
+  
     console.log(`${bot.user.tag} is now online!`)
     
 })
 
 bot.on('message', async message =>{
-  
+  //message.delete()
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
 if(message.author.bot) return;
 
