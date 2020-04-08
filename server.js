a325b35ccae67713af329687d7f51b21430c23a7
Glitch (hello-express) <none>
2020-04-08 05:43:36 +0000

    💸❄️ Checkpoint ./server.js:7452003/1128

--- server.js
+++ server.js
@@ -638,13 +638,15 @@ bot.on('message', async message =>{
     .setTimestamp()
     message.channel.send(embed)
   }
-  if (message.content.startsWith(prefix + "marry")) {
+   if (message.content.startsWith(prefix + "marry")) {
     const user = message.mentions.members.first()
     if (!user) return message.channel.send("Please state a person you would like to marry!");
-    const message = new Discord.MessageEmbed()
     if (user) return message.channel.send(`⛪ <@${message.author.id}> ** has proposed to**` + ` ${user} 💍\n💍 ${user.user.username}` + ", Do you accept? 🔔\n**Type `yes` to accept or `no` to decline.**")
-    await (message.content.startsWith().includes("yes")) message.channel.send() 
-  }
+    await (message.content.includes("yes")) {
+     if(message.user(user)){
+      message.channel.send(`Congratulations to <@${message.author.id}> & <@${user}> on their marriage!`);
+        }
+    }
   if (message.content.toLowerCase().includes('poll')) {
       if (message.channel.id !== '607042156368101437') return
         message.react('👍')
