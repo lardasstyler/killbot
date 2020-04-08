04e42e9e0828cb1c60264f6f0556d5436d4a9e7b
Glitch (hello-express) <none>
2020-04-08 04:45:17 +0000

    🌦✌️ Checkpoint ./server.js:7452003/311

--- server.js
+++ server.js
@@ -642,10 +642,9 @@ bot.on('message', async message =>{
     const user = message.mentions.members.first()
     if (!user) return message.channel.send("Please state a person you would like to marry!");
     if (user) return message.channel.send(`⛪ <@${message.author.id}> ** has proposed to**` + ` ${user} 💍\n💍 ${user.user.username}` + ", Do you accept? 🔔\n**Type `yes` to accept or `no` to decline.**")
-    await (message.content === "yes){
-
-
-}
+    await (message.content.includes("yes")) {
+      
+    }
   }
   if (message.content.toLowerCase().includes('poll')) {
       if (message.channel.id !== '607042156368101437') return
