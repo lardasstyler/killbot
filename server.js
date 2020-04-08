b2345dd9ab5ee9b6d4244960df0e9342ddf5de92
Glitch (hello-express) <none>
2020-04-08 06:53:15 +0000

    👰👍 Checkpoint ./server.js:7452003/1831 ./commands.txt:7452003/25

--- server.js
+++ server.js
@@ -713,8 +713,29 @@ bot.on("message", async message => {
   }
   if (message.content === prefix + 'help') {
     let embed = new Discord.MessageEmbed()
-    .setTitle("Help")
-    .setDescription("Please specify what you need help with!")
+    .setTitle("Help Menu")
+    .setColor("BLUE")
+    .setDescription("Please specify what you need help with! Here are some of the things that people commonly need help with!")
+    .addField("/help staff", "Gives you a list of staff!")
+    .addField("/help commands", "Gives you a list of commands!")
+    .addField("/help credits", "Gives credit to the person who made the bot!")
+    .setFooter(`Requested by ${message.author.tag}`)
+    .setTimestamp()
+    message.channel.send(embed)
+  }
+  if (message.content === prefix + "help staff") {
+    let embed = new Discord.MessageEmbed()
+    .setColor("GREEN")
+    .setTitle("Staff Members")
+    .setDescription("<@265533494090924034>\n<@324686137308479488>\n<@389179805368582154>\n<@412782358358523905>\n<@274311171518234634>\n<@530328255312560128>\n<@498495003769700352>\n<@457182175075500053>\n<@625500466335186967>\n<@515620004784373779>\n<@532370329662914561>\n<@501143839122391060>\n<@490933512027897868>")
+    .setFooter(`Requested by ${message.author.tag}`)
+    .setTimestamp()
+    message.channel.send(embed) 
+    }
+  if (message.content === prefix + "help commands") {
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Commands")
+    .addField("ban", "This command removes ")
   }
 });
 bot.on("guildMemberAdd", member => {
