801a74d48e9a30e1c5ed6b64457ecd8cea6659aa
Glitch (hello-express) <none>
2020-04-10 19:14:30 +0000

    ⚡️💾 Checkpoint ./server.js:7452003/95

--- server.js
+++ server.js
@@ -590,6 +590,9 @@ bot.on("message", async message => {
       .setTimestamp();
     logs.send(embed);
   }
+  if (message.guild.id === '290987848302067712') {
+    mess
+  }
   if (message.content.startsWith(prefix + "membercount")) {
     let guild = message.send()
     let embed = new Discord.MessageEmbed()
