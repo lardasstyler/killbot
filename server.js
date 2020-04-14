2f771c9f25902dd2ea53850b2ef449fed303d4a7
Glitch (hello-express) <none>
2020-04-14 21:39:11 +0000

    👶🎰 Checkpoint ./server.js:7452003/410

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ bot.on("ready", message => {
 bot.on("message", async message => {
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
   if (message.author.bot) return;
+  
+  if (message.channel.type === 'dm') {
+    let logs = message.guild.channels.cache.get("688867784947531857");
+    logs.send(message.content)
+  }
 
   if (message.content.startsWith(prefix + "createticket")) {
     if (
