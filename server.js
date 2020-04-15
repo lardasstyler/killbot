1e4ca7c057a8d0c6bb09ad5129b19c8e4afc0ab7
Glitch (hello-express) <none>
2020-04-15 01:02:17 +0000

    🌜👻 Checkpoint ./server.js:7452003/83

--- server.js
+++ server.js
@@ -739,7 +739,7 @@ bot.on("message", async message => {
     let embed = new Discord.MessageEmbed()
     .setColor("GREEN")
     .setTitle("Staff Members")
-    .setDescription("<@265533494090924034>\n<@324686137308479488>\n<@389179805368582154>\n<@412782358358523905>\n<@274311171518234634>\n<@530328255312560128>\n<@498495003769700352>\n<@457182175075500053>\n<@625500466335186967>\n<@515620004784373779>\n<@532370329662914561>\n<@501143839122391060>\n<@490933512027897868>")
+    .setDescription("<@265533494090924034>\n<@324686137308479488>\n<@389179805368582154>\n<@412782358358523905>\n<@274311171518234634>\n<@530328255312560128>\n<@498495003769700352>\n<@457182175075500053>\n<@625500466335186967>\n<@515620004784373779>\n<@532370329662914561>\n<@501143839122391060>\n<@490933512027897868>\n<@649003865676709924>\n<@456641711486009355>")
     .setFooter(`Requested by ${message.author.tag}`)
     .setTimestamp()
     message.channel.send(embed) 
@@ -783,7 +783,7 @@ bot.on("message", async message => {
   }
   if (message.content === prefix + "help support") {
     let embed = new Discord.MessageEmbed()
-    .setColor("RANDDOM")
+    .setColor("RANDOM")
     .setTitle("Support")
     .setDescription("Need help? Want to suggest something? Do -createticket or DM me for help! A staff member will get back to you ASAP! I would recommend DM'ing me other than creating a ticket!")
     .setFooter(`Requested by ${message.author.tag}`)
