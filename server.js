3e7ea50649dfe4d6769d1f40a61f51ca125d10e3
Glitch (hello-express) <none>
2020-04-17 06:07:11 +0000

    🛣👠 Checkpoint ./server.js:7452003/548

--- server.js
+++ server.js
@@ -25,6 +25,26 @@ bot.user.setActivity(game,{
   });
 }, 10000)
 });
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) =>{
+  if(err) console.log(err)
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0) {
+   return console.log("Could not find commands!");
+  }
+  
+  jsfile.forEach((f, i) =>{
+    let pull = require(`./commands/${f}`)
+    bot.commands.set(pull.config.name, pull);
+    pull.config.aliases.forEach(alias =>{
+      bot.aliases.set(alias, pull.config.name)
+    })
+  })
+})
 
 bot.on("message", async message => {
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
