e5dfdd175d90e568576054412a7e96386ff4b145
Glitch (hello-express) <none>
2020-04-18 03:44:54 +0000

    🛥💂 Checkpoint ./commands/economy.js:7452003/348

--- commands/economy.js
+++ commands/economy.js
@@ -1,2 +1,12 @@
 const db = require('quick.db');
-const Dsi
+const Discord = require('discord.js');
+const prefix = '-';
+module.exports = {
+  name: "test",
+  aliases: ["idk", "yet"],
+  run: async (bot, message, args) =>{
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    const dad = message.content.slice(prefix.length).trim().split(/ +/g);
+  }
+}
