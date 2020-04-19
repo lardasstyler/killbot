adde6af04871c2f28de070512882d03f7de90212
Glitch (hello-express) <none>
2020-04-19 04:17:57 +0000

    🌆🔦 Checkpoint ./commands/play.js:7452003/201

---
+++ commands/play.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+module.exports = {
+  name: "play",
+  aliases: [],
+  run: async (bot, message, args) =>{
+    
+  }
+}
