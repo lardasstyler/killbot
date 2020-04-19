856244e9f46f3ccf4b09745fc4a615462226d6e1
Glitch (hello-express) <none>
2020-04-19 12:10:58 +0000

    📊🏩 Checkpoint ./commands/8ball.js:7452003/1014

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,8 +1,19 @@
 const Discord = require('discord.js');
+const prefix = '-';
 module.exports = {
   name: "8ball",
   aliases: [],
   run: async (bot, message, args) =>{
-    
+     let errorEmbed1 = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("Error!")
+    .setDescription("You don't have a second argument!")
+    .setFooter("Having problems? Contact ty#6653!")
+    .setTimestamp()
+    if(!args[1]) return message.channel.send(errorEmbed1)
+    var choices = ["Yes", "No", "Maybe", "Probably", "Probably not"];
+
+    var output = choices[Math.floor(Math.random() * choices.length)];
+    message.channel.send(`<@${message.author.id}> => ${output}`);
   }
 }
