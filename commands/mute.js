edeef41f9742d2706240d1e3eddf2e89054d39f0
Glitch (hello-express) <none>
2020-04-20 00:17:58 +0000

    🐓🌛 Checkpoint ./commands/mute.js:7452003/739

--- commands/mute.js
+++ commands/mute.js
@@ -1,3 +1,18 @@
 const Discord = require('discord.js');
 const ms = require('ms');
 const prefix = '-';
+module.exports = {
+  name: "mute",
+  aliases: [],
+  run: async (bot, message, args) =>{
+    let mutee = message.mentions.members.first();
+    let errorEmbed = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("Error!")
+    .setDescription("You did not specify a user to mute therefore, nobody was muted!")
+    .setFooter("Having problems? Contact ty#6653!")
+    .setTimestamp()
+    if(!mutee) return message.channel.send(errorEmbed)
+    if(mutee.hasPermission("ADMINISTRATOR")) 
+  }
+}
