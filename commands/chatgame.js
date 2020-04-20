c8157cabcf22b83760868afb1523421f8f4531b2
Glitch (hello-express) <none>
2020-04-20 09:59:59 +0000

    🌏👑 Checkpoint ./commands/chatgame.js:7452003/607 ./commands/questions.json:7452003/299

--- commands/chatgame.js
+++ commands/chatgame.js
@@ -15,12 +15,12 @@ const item = quiz[Math.floor(Math.random() * quiz.length)];
 const filter = response => response.content.toLowerCase() === item.correct_answer.toLowerCase();
     
 let game = new Discord.MessageEmbed().setTitle('A wild chat game appeared!');
-if (item.type === 'boolean') game.setDescription(`TRUE OR FALSE: ${item.question}`).setFooter(`If you get the answer in the next 5 minutes, you get 250 coins! | Difficulty: ${item.difficulty}`).setColor('RANDOM');
-else game.setDescription(`${item.question}`).setFooter(`If you get the answer in the next 5 minutes, you get 250 coins! | Difficulty: ${item.difficulty}`).setColor('RANDOM')
+if (item.type === 'boolean') game.setDescription(`TRUE OR FALSE: ${item.question}`).setFooter(`You have the next 5 minutes to answer this question! | Difficulty: ${item.difficulty}`).setColor('RANDOM');
+else game.setDescription(`${item.question}`).setFooter(`You have the next 5 minutes to answer this question! | Difficulty: ${item.difficulty}`).setColor('RANDOM')
     bot.channels.cache.get('569682953148432394').send(game).then((lastEmbed) => {
  let collector = lastEmbed.channel.createMessageCollector(filter, { max: 1, time: 280000, errors: ['time'] })
 		collector.on('collect', collected => {
-bot.channels.cache.get('569682953148432394').send(`${collected.author} got the correct answer!`)
+     bot.channels.cache.get('569682953148432394').send(`${collected.author} got the correct answer!`)
   })
 		collector.on('end', collected => {
       if (collected.size === 0) {
