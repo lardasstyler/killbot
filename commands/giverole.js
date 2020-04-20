5868bee52f5703237880e4c94569c7ad648dc66b
Glitch (hello-express) <none>
2020-04-20 09:13:04 +0000

    🎊👣 Checkpoint ./commands/giverole.js:7452003/680

--- commands/giverole.js
+++ commands/giverole.js
@@ -1,9 +1,12 @@
 const Discord = require('discord.js');
+const prefix = '-';
 module.exports = {
   name: "giverole",
   aliases: " ",
   description: " ",
   run: async (bot, message, args) =>{
-    
+    let roles = message.mentions.roles.first();
+    let user = message.mentions.members.first();
+    await(user.roles.add(roles.id)); 
   }
 }
