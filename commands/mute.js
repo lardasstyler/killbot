adc0e11825c3ff93394c4246a655704c6f82bc55
Glitch (hello-express) <none>
2020-04-20 02:40:57 +0000

    🙈🍲 Checkpoint ./commands/mute.js:7452003/1615

--- commands/mute.js
+++ commands/mute.js
@@ -6,6 +6,15 @@ module.exports = {
   aliases: [],
   run: async (bot, message, args) =>{
     let mutee = message.mentions.members.first();
+    const women = message.content.slice(prefix.length).trim().split(/ +/g);
+    let reason = women.slice(3).join(' ') 
+    let errorEmbed7 = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("Error!")
+    .setDescription("You did not specify a reason to mute this user therefore, nobody was muted!")
+    .setFooter("Having problems? Contact ty#6653!")
+    .setTimestamp()
+    if(!reason) return message.channel.send(errorEmbed7);
     let errorEmbed = new Discord.MessageEmbed()
     .setColor("#FF0000")
     .setTitle("Error!")
@@ -22,7 +31,7 @@ module.exports = {
     if(mutee.hasPermission("ADMINISTRATOR")) return message.channel.send(errorEmbed1)
     let muterole = message.guild.roles.cache.get("674390381856686099");
     let verifiedrole = message.guild.roles.cache.get("581580272399679595");
-    let mutetime = args[1];
+    let mutetime = women.slice(2).join(' ')
     let errorEmbed2 = new Discord.MessageEmbed()
     .setColor("#FF0000")
     .setTitle("Error!")
@@ -33,9 +42,10 @@ module.exports = {
     
     await(mutee.roles.add(muterole.id));
     await(mutee.roles.remove(verifiedrole.id))
+    message.delete()
     let chatEmbed = new Discord.MessageEmbed()
     .setColor("RANDOM")
-    .setDescription(`**${mutee.user.tag} was muted!**`)
+    .setDescription(`***${mutee.user.tag} is muted., ${reason}***`)
     message.channel.send(chatEmbed)
     let logs = message.guild.channels.cache.get("456272126756782101");
     let embed = new Discord.MessageEmbed()
@@ -44,9 +54,19 @@ module.exports = {
       .addField("User:", `${mutee.user.tag}`, true)
       .addField("Moderator:", `<@${message.author.id}>`, true)
       .addField("Time:", `${ms(mutetime)} milisecs`, true)
+      .addField("Reason:", reason, true)
       .setFooter(`USERS ID: ${mutee.id}`)
       .setTimestamp();
     logs.send(embed)
+   let muteEmbed = new Discord.MessageEmbed()
+    .setTitle("⚠️ You were muted! ⚠️")
+    .setColor("BLUE")
+    .addField("Server:", message.guild.name, true)
+    .addField("Moderator:", `${message.author.tag}`, true)
+   .addField("Time:", `${ms(mutetime)} milisecs`, true)
+    .addField("Reason:", `${reason}`, true)
+    .setTimestamp()
+    mutee.send(muteEmbed)
     
     setTimeout(function(){
       mutee.roles.remove(muterole.id)
