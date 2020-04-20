fb488552f811f7340931c2bea8834da42bdb7cd8
Glitch (hello-express) <none>
2020-04-20 03:40:58 +0000

    🚜🏊🏼 Checkpoint ./commands/announce.js:7452003/781

--- commands/announce.js
+++ commands/announce.js
@@ -3,7 +3,7 @@ const prefix = '-';
 module.exports = {
   name: "announce",
   aliases: [],
-  run: async (bot, args, message) =>{
+  run: async (bot, message, args) =>{
     let errorEmbed1 = new Discord.MessageEmbed()
     .setColor("#FF0000")
     .setTitle("Error!")
@@ -12,6 +12,7 @@ module.exports = {
     .setTimestamp()
     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(errorEmbed1)
     let announce = message.guild.channels.cache.get("532779225254723584");
+    let user = message.mentions.users.first();
     const women = message.content.slice(prefix.length).trim().split(/ +/g);
     let ann = women.slice(1).join(' ') 
       let errorEmbed = new Discord.MessageEmbed()
@@ -20,6 +21,13 @@ module.exports = {
     .setDescription("You didn't state anything to announce!")
     .setFooter("Having problems? Contact ty#6653!")
     .setTimestamp()
-    if(!ann) return message.channel.send("Please state a reason to warn this user!");
+    if(!ann) return message.channel.send(errorEmbed);
+    let announceEmbed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setAuthor("Announcement", message.guild.iconURL())
+    .setDescription(ann)
+    .setFooter(`Announced by ${message.author.tag}`)
+    .setTimestamp()
+    announce.send(announceEmbed)
   }
 }
