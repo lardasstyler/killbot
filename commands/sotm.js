859bdc837f425264f243067a64e77b3b77f228ff
Glitch (hello-express) <none>
2020-04-22 10:34:54 +0000

    🚁⏰ Checkpoint ./commands/sotm.js:7452003/349

---
+++ commands/sotm.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: "sotm",
+  aliases: [],
+  run: async (bot, message, args) =>{
+    const embed = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setDescription
+  }
+}
