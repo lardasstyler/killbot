eb00848005d517a4a6aa8dbd183fda23cd073ec9
Glitch (hello-express) <none>
2020-04-22 09:59:54 +0000

    💂🍂 Checkpoint ./commands/questions.json:7452003/388 ./server.js:7452003/1053

--- server.js
+++ server.js
@@ -59,14 +59,10 @@ const args = message.content.slice(prefix.length).trim().split(/ +/g);
    message.channel.send("don't come back")
   }
 });
-var j = schedule.scheduleJob('0 30 * * * *', function(){
-   let commandFile = require("./commands/chatgame.js");
+setInterval(() =>{
+let commandFile = require("./commands/chatgame.js");
        commandFile.run(bot);
- })
-var j = schedule.scheduleJob('0 0 * * * *', function(){
-   let commandFile = require("./commands/chatgame.js");
-       commandFile.run(bot);
- })
+//}, 1000)
 
 
 
