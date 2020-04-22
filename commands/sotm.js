e89d7229d1b751de2363e18e3aa47b51aad586d1
Glitch (hello-express) <none>
2020-04-22 22:19:58 +0000

    🎠🐸 Checkpoint ./commands/sotm.js:7452003/779

--- commands/sotm.js
+++ commands/sotm.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 module.exports = {
-  name: "sotm",
+  name: "motm",
   aliases: [],
   run: async (bot, message, args) =>{
     const embed = new Discord.MessageEmbed()
     .setColor('RANDOM')
-    .setDescription("If you ask for members to vote for you, you will be disqualified!")
+    .setAuthor("MOTM", message.guild.iconURL())
+    .setDescription("Welcome to Member of the Month! This is similar to **SOTM but with members. We have had this contest in the past but removed it because we ran out of members to use for MOTM! Now, we have a lot more member than before!\n \n **Explanation:** It's pretty simple, a staff member chooses a member every month to be the MOTM! The MOTM will get a role called 'Member of the Month' and will keep it until we elect the next member!")
+    .setFooter('PigPig and Raging’s Discord Server | MOTM')
     message.channel.send(embed)
   }
 }
