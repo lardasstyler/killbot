c782e1fab50043d97933d56fd150e4757d8277c3
Glitch (hello-express) <none>
2020-04-22 23:19:00 +0000

    🚒🍡 Checkpoint ./commands/kick.js:7452003/148

--- commands/kick.js
+++ commands/kick.js
@@ -6,13 +6,13 @@ module.exports = {
     if (!message.member.hasPermission('KICK_MEMBERS')) return;
     let user = message.mentions.members.first() || message.guild.members.get(args[0])
     let channel = message.guild.channels.cache.get("456272126756782101");
-    let errorEmbed = new Discord.MessageEmbed()
     let reason = args.slice(1).join(" ")
+    let errorEmbed = new Discord.MessageEmbed()
     .setColor('#FF0000')
     .setDescription("<:exx:702650687145902103> The user you chose is a staff member, I can't kick them!")
     if (user.hasPermission('MANAGE_MESSAGES')) return message.channel.send(errorEmbed)
     let kickEmbed = new Discord.MessageEmbed()
     .setDescription(`**${user.user.tag} was kicked***`)
-    await user.kick(), message.channel.send(kickEmbed)
+    await user.kick({reason: reason}), message.channel.send(kickEmbed)
   }
 }
