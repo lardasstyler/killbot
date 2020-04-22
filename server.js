aadf447837269f71bfaf098c22f17ff25b7754b4
Glitch (hello-express) <none>
2020-04-22 11:22:55 +0000

    ⛈🌋 Checkpoint ./commands/questions.json:7452003/381 ./server.js:7452003/405

--- server.js
+++ server.js
@@ -59,12 +59,14 @@ const args = message.content.slice(prefix.length).trim().split(/ +/g);
    message.channel.send("don't come back")
   }
 });
-setInterval(() =>{
-let commandFile = require("./commands/chatgame.js");
+var j = schedule.scheduleJob('0 30 * * * *', function(){
+   let commandFile = require("./commands/chatgame.js");
        commandFile.run(bot);
-
-}, 900000)
-
+ })
+var j = schedule.scheduleJob('0 0 * * * *', function(){
+   let commandFile = require("./commands/chatgame.js");
+       commandFile.run(bot);
+ })
 
 
 bot.on("message", async message => {
