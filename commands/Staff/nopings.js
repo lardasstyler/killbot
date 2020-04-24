34eaaa1e5396e53301df71d21c1293a28b0293a1
Glitch (hello-express) <none>
2020-04-24 18:45:39 +0000

    👵🚿 Checkpoint ./commands/Staff/pings.js:7452003/10 ./commands/Staff/nopings.js:7452003/562 ./commands/Helpful/help.js:7452003/403

--- commands/Staff/nopings.js
+++ commands/Staff/nopings.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports = {
-  name: "pings",
+  name: "nopings",
   aliases: [],
   run: async (bot, message, args) =>{
     if(!message.member.hasPermission('MANAGE_MESSAGES')) return;
@@ -8,7 +8,7 @@ module.exports = {
     let pingrole = message.guild.roles.cache.get("617459453067001856")
     let nopingrole = message.guild.roles.cache.get("703311925450375249")
     
-    await(message.member.roles.add(pingrole.id))
-    await(message.member.roles.remove(nopingrole.id)), message.channel.send(`\`${message.author.tag}\` was given "\`Pingable Staff\`"!`)
+    await(message.member.roles.remove(pingrole.id))
+    await(message.member.roles.add(nopingrole.id)), message.channel.send(`\`${message.author.tag}\` was given "\`Non-Pingable Staff\`"!`)
   }
 }
