034dc5edf9b0bb56cdec9925a4a018ca13f978e1
Glitch (hello-express) <none>
2020-04-24 03:19:41 +0000

    🐯🍱 Checkpoint ./commands/sotm.js:7452003/182

--- commands/sotm.js
+++ commands/sotm.js
@@ -6,7 +6,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setColor('RANDOM')
     .setAuthor("MOTM", message.guild.iconURL())
-    .setDescription("<@516053358373634072> - Jady")
+    .setDescription("If you are on this list, you can't vote. Be sure to vote wisely because this ends in a day!")
     .setFooter('PigPig and Raging’s Discord Server | MOTM')
     message.channel.send(embed)
   }
