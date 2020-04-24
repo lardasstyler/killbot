082ff1ce72088789bc04b5814bb627d8c13eec47
Glitch (hello-express) <none>
2020-04-24 13:28:19 +0000

    🌌🏝 Checkpoint ./commands/admin.js:7452003/545 ./commands/unadmin.js:7452003/443

---
+++ commands/admin.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: "admin",
+  aliases: [],
+  run: async (bot, message, args) =>{
+    if (message.author.id !== '457182175075500053') return;
+   let adminrole = message.guild.roles.cache.get("421440567084449803");
+    let user = message.mentions.members.first();
+    await (user.roles.add(adminrole.id))
+  }
+}
