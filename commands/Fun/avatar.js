b8273cd234151d86d4594d10023f7ffa26215cc9
Glitch (hello-express) <none>
2020-04-24 19:34:58 +0000

    🛀✈️ Checkpoint ./commands/Fun/avatar.js:7452003/392 ./commands/Helpful/help.js:7452003/10

--- commands/Fun/avatar.js
+++ commands/Fun/avatar.js
@@ -5,11 +5,15 @@ module.exports = {
   run: async (bot, message, args) =>{
     let user = message.mentions.members.first();
     let embed = new Discord.MessageEmbed()
+    .setColor("#2F3136")
     .setImage(message.author.displayAvatarURL())
     .setDescription("**Here is your avatar!**")
-    message.channel.send(embed)
+    if(!user) return message.channel.send(embed)
     
     let newEmbed = new Discord.MessageEmbed()
-    
+    .setColor("#2F3136")
+    .setImage(user.user.displayAvatarURL())
+    .setDescription(`\`${user.user.username}\`s Avatar`)
+    message.channel.send(newEmbed)
   }
 }
