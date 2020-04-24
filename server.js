5e0a1467a054c5df2b58be16ed20366d8ce2ead0
Glitch (hello-express) <none>
2020-04-24 15:10:18 +0000

    ✈️👽 Checkpoint ./server.js:5734656/1133 ./package.json:5734656/1436

--- server.js
+++ server.js
@@ -19,19 +19,28 @@ const prefix = '-';
 bot.on("ready", message => {
 
 })
-const init = async message =>{
-  fs.readdir(`./commands`, (err, files) =>{
-    if(err) return console.log(err);
-    files.forEach(file =>{
-      const props = require(`./commands/${file}`);
-      bot.commands.set(props.name, props);
-      bot.aliases.forEach(alias => bot.aliases.set(alias, props.name));
-      console.log(`bruh ${file} moment`)
-    })
-  })
-}
-init();
-
+const ascii = require("ascii-table");
+let table = new ascii("Commands");
+table.setHeading("Command", "Load status");
+  fs.readdirSync("./commands/").forEach(dir => {
+    
+    const commands = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+    
+    for (let file of commands) {
+      let pull = require(`./commands/${dir}/${file}`);
+    
+      if (pull.name) {
+        bot.commands.set(pull.name, pull);
+        table.addRow(file, '✅');
+      } else {
+        table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+        continue;
+      }
+    
+      if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name));
+    }
+  });
+  console.log(table.toString());
 bot.on('message', (message) => {
   if (message.author.bot) return;
   if (message.content.indexOf(prefix) !== 0) return;
