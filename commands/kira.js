41cc3ef9cee56f8f6362c1e3ed0fe345b654acd7
Glitch (hello-express) <none>
2020-04-24 12:40:20 +0000

    🏚🍢 Checkpoint ./commands/kira.js:7452003/85

--- commands/kira.js
+++ commands/kira.js
@@ -4,6 +4,8 @@ module.exports = {
   aliases: [],
   run: async (bot, message, args) =>{
     let embed = new Discord.MessageEmbed()
-    .setThumbnail("https://cdn.discordapp.com/attachments/607107189571715099/643229245916839937/image0.jpg")
+    .attach("https://cdn.discordapp.com/attachments/607107189571715099/643229245916839937/image0.jpg")
+    
+    message.channel.send(embed)
   }
 }
