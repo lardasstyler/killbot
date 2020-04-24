0b0ceb02e49e688bf5ac40dadb7279f7f8756b54
Glitch (hello-express) <none>
2020-04-24 16:58:17 +0000

    🙈🚿 Checkpoint ./commands/Staff/ban.js:7452003/276

--- commands/Staff/ban.js
+++ commands/Staff/ban.js
@@ -5,23 +5,35 @@ module.exports = {
   run: async (bot, message, args) => {
     if (!message.member.hasPermission("BAN_MEMBERS")) return;
     //Defining Stuff
+    
+    
     const user = message.mentions.members.first();
     let logs = message.guild.channels.cache.get("456272126756782101");
     let reason = args.slice(2).join(" ")
+    
+    
     //Command
-    let errorEmbed = new Discord.MessageEmbed()
+    
+    let banEmbed = new Discord.MessageEmbed()
     .setColor("#2F3136")
     .setTitle("**Banning a User**")
     .setDescription("To ban a user, do `-ban [user] (reason)`\n \n You can only ping the user if you want to ban them.")
-    if (!user) return message.channel.send(errorEmbed);
-        message.delete();
-    await user.ban(), message.channel.send(`${user} was banned!`);
+    if (!user) return message.channel.send(banEmbed);
+    
+    
+    message.delete();
+    
+    
+    await user.ban(), message.channel.send(`\`${user.user.tag}\` was banned!`);
+    
+    
     const embed = new Discord.MessageEmbed()
-      .setColor("#E36947")
+      .setColor("#2F3136")
       .setTitle("Banned User")
       .addField("User:", `<@${user.id}>`, true)
-      .addField("Mod:", `<@${message.author.id}>`, true)
-      .setFooter(`USERS ID: ${user.id}`)
+      .addField("Moderator:", `<@${message.author.id}>`, true)
+      .addField("Reason:", `${reason}`, true)
+      .setFooter(`ID: ${user.id}`)
       .setTimestamp();
     logs.send(embed);
   }
