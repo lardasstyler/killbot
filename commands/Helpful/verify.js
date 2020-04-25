df78ee146efb07e67e7eb59a5e473e54333deeca
Glitch (hello-express) <none>
2020-04-25 10:29:48 +0000

    🎳💶 Checkpoint ./commands/Helpful/verify.js:7452003/1379

--- commands/Helpful/verify.js
+++ commands/Helpful/verify.js
@@ -3,8 +3,13 @@ module.exports = {
   name: "verify",
   aliases: [],
   run: async (bot, message, args) =>{
-    let verified = message.guild.roles.cache.get("581580272399679595")
-    if(message.member.hasRole("role")) return message.channel.send("test")
-
+  if (message.member.roles.cache.some(role => role.name === 'Verified ✔')) return;
+  let verifiedrole = message.guild.roles.cache.get("581580272399679595");
+  await(message.member.roles.add(verifiedrole.id))
+    let embed = new Discord.MessageEmbed()
+    .setTitle("**Verified**")
+    .setColor("#2F3136")
+    .setDescription(`You are now verified in ${message.guild.name}! Please read <#561008923453423657> to have the best experience!`)
+    message.author.send(embed)
   }
 }
