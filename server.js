cd003ea013c5ee257ed9f89300e3af7ba1f417d5
Glitch (hello-express) <none>
2020-04-25 02:53:48 +0000

    💅🏍 Checkpoint ./server.js:7452003/164

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const fs = require('fs');
 const prefix = '-';
 
 bot.on("ready", message => {
-
+bot.user.setActivity("i hate it when youtube just decides to shuffle to a random song in my playlist one last time and turns shuffle off", { type: "PLAYING"})
 })
 const ascii = require("ascii-table");
 let table = new ascii("Commands");
