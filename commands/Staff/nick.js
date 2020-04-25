ff0d3b42656affb069ba2891f7b0a976589c6221
Glitch (hello-express) <none>
2020-04-25 05:37:48 +0000

    🐜🗻 Checkpoint ./commands/Staff/nick.js:7452003/1327 ./commands/Staff/unjail.js:7452003/578

---
+++ commands/Staff/nick.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: "nick",
+  aliases: [],
+  run: async (bot, message, args) =>{
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return;
+    
+    let user = message.mentions.members.first();
+    let nickname = args.slice(1).join(" ")
+      let nickEmbed = new Discord.MessageEmbed()
+    .setColor("#2F3136")
+    .setTitle("**Changing a Users Nickname**")
+    .setDescription("To change a users nickname, do `-nick [user] (nickname)`. \n \n You can only mention a user to change their nickname.")
+    if(!user) return message.channel.send(nickEmbed)
+  
+    
+    if(!nickname) return user.removeNickname()
+    
+    user.setNickname(nickname)
+  }
+}
