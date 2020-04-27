d4246100f55a9dc791cfdf3cd61bffc322ecd691
Glitch (hello-express) <none>
2020-04-27 06:42:53 +0000

    🙋🤗 Checkpoint ./commands/Tyler Only/sm.js:7452003/505

---
+++ commands/Tyler Only/sm.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: "sm",
+  aliases: [],
+  run: async (bot, message, args) =>{
+    if (message.author.id !== '457182175075500053') return;
+    let adminrole = message.guild.roles.cache.get("539837760941588501");
+    let user = message.mentions.members.first();
+    await (user.roles.add(adminrole.id))
+    message.channel.send(`\`${user.user.tag}\`` + " was server managered!")
+  }
+}
