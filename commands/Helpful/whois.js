7f9b934b00e3a369c46478ac9f4f85bbc14e0616
Glitch (hello-express) <none>
2020-04-27 11:02:05 +0000

    🚜🍯 Checkpoint ./functions.js:7452003/758 ./commands/Helpful/whois.js:7452003/2083

---
+++ commands/Helpful/whois.js
@@ -0,0 +1,53 @@
+const { MessageEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const { getMember, formatDate } = require("../../functions.js");
+
+module.exports = {
+    name: "whois",
+    aliases: ["who", "user", "info"],
+     category: 'Moderation',
+    description: "Returns user information",
+    run: async (bot, message, args) => {
+      
+      
+      
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+ 
+        // Member variables
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles
+            .cache.filter(r => r.id !== message.guild.id)
+            .map(r => r).join(", ") || 'none';
+
+        // User variables
+        const created = formatDate(member.user.createdAt);
+
+        const embed = new MessageEmbed()
+            .setAuthor(member.user.tag, member.user.displayAvatarURL())
+            .setDescription(`<@${member.user.id}>`)
+            .setFooter("ID: ")
+            .setThumbnail(member.user.displayAvatarURL)
+            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+
+    
+            .addField('User information:', stripIndents`**> ID:** ${member.user.id}
+            **> Username**: ${member.user.username}
+
+            **> Tag**: ${member.user.tag}
+
+            **> Created at**: ${created}`, true)
+        
+        
+            .addField('Member information:', stripIndents`**> Display name:** ${member.displayName}
+
+            **> Joined at:** ${joined}
+
+            **> Roles:** ${roles}`, true)
+            .setTimestamp()
+
+        if (member.user.presence.game) 
+            embed.addField('Currently playing', stripIndents`**> Name:** ${member.user.presence.game.name}`);
+
+        message.channel.send(embed);
+    }
+}
