558b3505b81005743fbc94c3a3d94eb8cf492e25
Glitch (hello-express) <none>
2020-04-28 14:24:01 +0000

    💖🌼 Checkpoint ./commands/Tyler Only/events.js:7452003/854

--- commands/Tyler Only/events.js
+++ commands/Tyler Only/events.js
@@ -4,11 +4,11 @@ module.exports = {
   aliases: [],
   run: async (bot, message, args) =>{
     if (message.author.id !== '457182175075500053') return;
-    const embed = new Discord.MessageEmbed()
-    .setColor('#2F3136')
-    .setAuthor("Verifying", message.guild.iconURL())
-    .setDescription("To verify, type `?verify`! You then will have access to the whole entire server! If the command isn't working, contact `ty#6653`!")
-    .setFooter('PigPig and Raging’s Discord Server | Verification')
-    message.channel.send(embed)
+ let embed = new Discord.MessageEmbed()
+.setTitle("Embeding")
+.setDescription("gay")
+.setFooter(`Requested by ${message.author.tag}`)
+.setColor("RANDOM")
+message.channel.send(embed)
   }
 }
