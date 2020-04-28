9aa7284b5e85d9b68ae9eeb0aab6da0a46d199da
Glitch (hello-express) <none>
2020-04-28 18:14:44 +0000

    ⏰🛳 Checkpoint ./commands/Staff/say.js:7452003/34 ./commands/Staff/nick.js:7452003/27

--- commands/Staff/Admins/say.js
+++ commands/Staff/Admins/say.js
@@ -4,7 +4,7 @@ module.exports = {
   name: "say",
   aliases: [],
   run: async (bot, message, args) =>{
-     if (!message.member.hasPermission("MANAGE_MESSAGES")) return;
+     if (!message.member.hasPermission("ADMINISTRATOR")) return;
     const argu = message.content.slice(prefix.length).trim().split(/ +/g);
     let say = argu.slice(1).join(' ') 
     message.channel.send(say)
