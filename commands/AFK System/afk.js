98144d40f994d003fb7b5ff3554de2f8ee2623fb
Glitch (hello-express) <none>
2020-04-29 23:57:02 +0000

    🍈🌥 Checkpoint ./commands/AFK System/afk.js:7452003/297

---
+++ commands/AFK System/afk.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: "afk",
+  aliases: [],
+  run: async (bot, message, args) =>{
+    //Defining
+    
+    let rafk = args.slice(0).join(" ")
+    
+    //Command
+  }
+}
