6a3a0ac49a844f9648627f652e84392c95a4819e
Glitch (hello-express) <none>
2020-04-30 19:14:45 +0000

    💺🐡 Checkpoint ./commands/Staff/servername.js:7452003/60

--- commands/Staff/servername.js
+++ commands/Staff/servername.js
@@ -3,6 +3,7 @@ module.exports = {
   name: "servername",
   aliases: [],
   run: async (bot, message, args)=>{
+    if (message.author.id !== '457182175075500053') return;
     if (!message.member.hasPermission("ADMINISTRATOR")) return;
     let name = args.slice(0).join(" ")
     await message.guild.setName(name)
