d579211899333f3b4b2aecb3db2683592763eced
Glitch (hello-express) <none>
2020-04-30 18:07:52 +0000

    📉🎆 Checkpoint ./commands/Tyler Only/hadmin.js:7452003/133 ./commands/Tyler Only/admin.js:7452003/133 ./commands/Tyler Only/sm.js:7452003/133 ./commands/Tyler Only/unadmin.js:7452003/133 ./commands/Tyler Only/unhadmin.js:7452003/133 ./commands/Tyler Only/unsm.js:7452003/134

--- commands/Tyler Only/unhadmin.js
+++ commands/Tyler Only/unhadmin.js
@@ -5,7 +5,7 @@ module.exports = {
   run: async (bot, message, args) =>{
     if (message.author.id !== '457182175075500053') return;
     let adminrole = message.guild.roles.cache.get("456258011124596746");
-    let user = message.mentions.members.first();
+      let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     await (user.roles.remove(adminrole.id))
     message.channel.send(`\`${user.user.tag}\`` + " was unhead admined!")
   }
--- commands/Tyler Only/unsm.js
+++ commands/Tyler Only/unsm.js
@@ -5,7 +5,7 @@ module.exports = {
   run: async (bot, message, args) =>{
     if (message.author.id !== '457182175075500053') return;
     let adminrole = message.guild.roles.cache.get("539837760941588501");
-    let user = message.mentions.members.first();
+     let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     await (user.roles.remove(adminrole.id))
     message.channel.send(`\`${user.user.tag}\`` + " was unserver managered!")
   }
