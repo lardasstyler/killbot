b0dcc5573482260ac90f1ce2f75451e718b43e9f
Glitch (hello-express) <none>
2020-05-02 21:50:17 +0000

    🌘🎊 Checkpoint ./commands/Tyler Only/events.js:7452003/446

--- commands/Tyler Only/events.js
+++ commands/Tyler Only/events.js
@@ -5,10 +5,10 @@ module.exports = {
   run: async (bot, message, args) =>{
     if (message.author.id !== '457182175075500053') return;
  let embed = new Discord.MessageEmbed()
-.setAuthor("Applications & Appeals", message.guild.IconURL)
-.setDescription("To apply or appeal your ban click me! If you are applying and you get denied, you have to wait at least **29-31** days to apply again. Be sure to not be lazy while applying and try your best! Good luck!")
+.setAuthor("Applications & Appeals", message.guild.iconURL())
+.setDescription("To apply or appeal your ban [click me](https://docs.google.com/forms/d/e/1FAIpQLSdF7XfZrLxTSe1rRBQZTgH_7PXuQxz4M6noL1kDTH--agbgIQ/viewform)! If you are applying and you get denied, you have to wait at least **29-31** days to apply again. Be sure to not be lazy while applying and try your best! Good luck!")
 .setFooter(`PigPig and Raging's Discord Server | Applications & Appeals`)
-.setColor("PINK")
+.setColor("#FF69B4")
 message.channel.send(embed)
   }
 }
