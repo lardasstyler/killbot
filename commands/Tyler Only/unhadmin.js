45bbd49e35caecc08a3644a11da4de4796d4eaa6
Glitch (hello-express) <none>
2020-05-12 21:09:33 +0000

    🚦⚽️ Checkpoint ./commands/Tyler Only/unadmin.js:7452003/36 ./commands/Tyler Only/unsm.js:7452003/36 ./commands/Tyler Only/unhadmin.js:7452003/36

--- commands/Tyler Only/unhadmin.js
+++ commands/Tyler Only/unhadmin.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "unhadmin",
   aliases: [],
   run: async (bot, message, args) =>{
-    if (message.author.id !== '457182175075500053') return;
+    if (message.author.id !== '648698528872398848') return;
     let adminrole = message.guild.roles.cache.get("456258011124596746");
       let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     await (user.roles.remove(adminrole.id))
--- commands/Tyler Only/unsm.js
+++ commands/Tyler Only/unsm.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "unsm",
   aliases: [],
   run: async (bot, message, args) =>{
-    if (message.author.id !== '457182175075500053') return;
+    if (message.author.id !== '648698528872398848') return;
     let adminrole = message.guild.roles.cache.get("539837760941588501");
      let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     await (user.roles.remove(adminrole.id))
