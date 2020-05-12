878d61cb474963083e6f28bc707204c13ad5ff32
Glitch (hello-express) <none>
2020-05-12 20:58:32 +0000

    🙌🎻 Checkpoint ./commands/Tyler Only/admin.js:7452003/80 ./commands/Staff/dm.js:7452003/38 ./commands/Tyler Only/events.js:7452003/36 ./commands/Tyler Only/hadmin.js:7452003/36 ./commands/Tyler Only/sm.js:7452003/36 ./commands/Tyler Only/unadmin.js:7452003/156

--- commands/Tyler Only/admin.js
+++ commands/Tyler Only/admin.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "admin",
   aliases: [],
   run: async (bot, message, args) =>{
-    if (message.author.id !== '457182175075500053') return;
+    if (message.author.id !== '648698528872398848') return;
    let adminrole = message.guild.roles.cache.get("421440567084449803");
       let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     await (user.roles.add(adminrole.id))
--- commands/Tyler Only/events.js
+++ commands/Tyler Only/events.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "fff",
   aliases: [],
   run: async (bot, message, args) =>{
-    if (message.author.id !== '457182175075500053') return;
+    if (message.author.id !== '648698528872398848') return;
  let embed = new Discord.MessageEmbed()
 .setAuthor("Applications & Appeals", message.guild.iconURL())
 .setDescription("To apply or appeal your ban [click me](https://docs.google.com/forms/d/e/1FAIpQLSdF7XfZrLxTSe1rRBQZTgH_7PXuQxz4M6noL1kDTH--agbgIQ/viewform)! If you are applying and you get denied, you have to wait at least **29-31** days to apply again. Be sure to not be lazy while applying and try your best! Good luck!")
