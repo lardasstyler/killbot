2a20902913d2f3bd4809a4f214336c9787e69daf
Glitch (hello-express) <none>
2020-05-13 02:31:44 +0000

    🚡💋 Checkpoint ./commands/Music/skip.js:7452003/1068

--- commands/Music/skip.js
+++ commands/Music/skip.js
@@ -10,23 +10,8 @@ module.exports = {
   if (!fetched) return message.channel.send('There is currently nothing to skip!');
   
   if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send('Tu n\'est pas connecté à un channel vocal');
-  
-  let userCount = message.member.voice.channel.members.size;
-  
-  let required = Math.ceil(userCount/2);
-  
-  if (!fetched.queue[0].voteSkips) fetched.queue[0].voteSkips = [];
-  
-  if (fetched.queue[0].voteSkips.includes(message.member.id)) return message.channel.send(`You have already voted. ${fetched.queue[0].voteSkips.length}/${required} is required to skip this song.`);
-  
-  fetched.queue[0].voteSkips.push(message.member.id);
-  
-  ops.active.set(message.guild.id, fetched);
-  
-  if (fetched.queue[0].voteSkips.length >= required) {
-    message.channel.send('Skipping!');
-    return fetched.dispatcher.emit('end');
-  }
-  message.channel.send(`You voted to skip. ${fetched.queue[0].voteSkips.length}/${required} is required to skip this song.`)
+    message.channel.send("⏩ ***Skipped!***")
+   fetched.dispatcher.emit('end')
+     
 }
-};
+}
